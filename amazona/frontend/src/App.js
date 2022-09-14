39ccb77411b5fc40e5c64d9ba4d157d74bab82e7
Hannah Gloria <eleonor.ragl@gmail.com>
2022-09-14 15:16:26 -0500

    initial

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App">
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
