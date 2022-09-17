0c13fbdd45d8a46baa0b044eee53eaa98a4ebe97
HannahGloria <33426405+HannahGloria@users.noreply.github.com>
2022-09-17 12:39:31 -0500

    dat

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -4,7 +4,16 @@ function App() {
       <header className="App">
         <a href="/">amazona</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured products</h1>
+        {data.products.map((product)=>(
+          <div>
+            <img src={product.image} alt={product.name}/>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,40 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg', //679px x 829829px
+            price: 120,
+            countInStock: 1,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+        name: 'Nike Slim pant',
+        slug: 'nike-slim-pant',
+        category: 'Shirts',
+        image:'/images/p2.jpg',
+        price: 120,
+        countInStock: 3,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product',
+        },
+        {
+    name: 'Adidas Fit Pant',
+    slug: 'Adidas-fit-pant',
+    category: 'Shirts',
+    image:'/images/p3.jpg',
+    price: 120,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 10,
+    description: 'high quality product',
+        },
+    ]
+};
+export default data;
